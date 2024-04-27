77105b3e5b8dcddff26648a1c393a28862292996
chrisanich <chrisanich@localhost>
2024-04-27 22:41:46 +0100

    First commit to the CA_2 project

---
+++ src/rugbysystem/RugbySystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rugbysystem;
+
+/**
+ *
+ * @author chrisanich
+ */
+public class RugbySystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
