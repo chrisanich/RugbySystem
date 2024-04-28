fe4d328ceae54a2ae89a5910e5b9a13b45d04962
chrisanich <chrisanich@localhost>
2024-04-28 22:28:06 +0100

    Corrections Team_Class (28.04.24)

--- src/rugbysystem/Team_Class.java
+++ src/rugbysystem/Team_Class.java
@@ -13,18 +13,20 @@ public class Team_Class {
     //necessary were invented.
     private int team_id;
     private String team_name;
+    private String team_coach;
     private String team_country;
     private String team_city;
     
     //We create a constructor (method) for this class, which allows us tho add different
     //attributes to it. So, every time we create an object with this class
     //it will have all the attributes inside it.
-    public Team_Class (int team_id, String team_name, String team_country, String team_city) {
+    public Team_Class (int team_id, String team_name, String team_coach, String team_country, String team_city) {
         //The next line add different attributes to every actor from the class
         //Coach. With this, we create atributes and assign its value when using
         //this method
         this.team_id = team_id;
         this.team_name = team_name;
+        this.team_coach = team_coach;
         this.team_country = team_country;
         this.team_city = team_city;
     }
@@ -39,6 +41,11 @@ public class Team_Class {
     public String getTeamName () {
         //This method returns the current state(value) of "this.team_name" 
         return team_name;
+    }
+    
+    public String getTeamCoach () {
+        //This method returns the current state(value) of "this.team_name" 
+        return team_coach;
     }
     
     public String getTeamCountry () {
