19f5286bcdc2bfe6e7abafbd0160104356609afa
chrisanich <chrisanich@localhost>
2024-05-02 19:40:35 +0100

    I renamed the package to CA_2 as asked.

--- src/CA_2/Menu_Imp.java
+++ src/CA_2/Menu_Imp.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package rugbysystem;
+package CA_2;
 
 import java.util.Scanner;
-import rugbysystem.Menu.MenuOption;
-import rugbysystem.Method;
+import CA_2.Menu.MenuOption;
+import CA_2.Method;
 
 /**
  *
